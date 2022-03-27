6a58042def0f654d95684bdaa64ea34716194dc7
GrzegorzP <pietruszka.grzegorz@gmail.com>
2022-03-27 19:59:08 +0100

    created Section component

---
+++ pages/components/section/Section.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  background: ${(props) =>
+    props.backgroundColour ? props.backgroundColour : "#fff"};
+  width: 100%;
+  padding: 2em 0;
+`;
+
+export default function Section({ children, backgroundColour }) {
+  return (
+    <StyledSection backgroundColour={backgroundColour}>
+      {children}
+    </StyledSection>
+  );
+}
