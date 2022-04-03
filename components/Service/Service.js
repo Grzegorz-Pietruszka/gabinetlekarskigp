636f2f4b42a216ba1abf291d1b56a7ceab9af7f2
GrzegorzP <pietruszka.grzegorz@gmail.com>
2022-04-03 19:45:40 +0100

    updated Service component

--- components/Service/Service.js
+++ components/Service/Service.js
@@ -1,10 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledIndex = styled.h3`
+  color: blue;
+`;
+
+const StyledTitle = styled.h3`
+  font-family: "Playfair Display", serif;
+`;
 
 export default function Service({ service, index }) {
   return (
     <>
-      <h2>{index}</h2>
-      <h2>{service}</h2>
+      <StyledIndex>{index}</StyledIndex>
+      <StyledTitle>{service}</StyledTitle>
     </>
   );
 }
